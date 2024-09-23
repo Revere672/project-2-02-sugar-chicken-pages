350fe5ae33e4dd4f1e45eb79091e62ecad00da03
theejoshuajg <theejoshuajg@tamu.edu>
2024-09-23 18:20:52 -0500

    edit script to add day spikes
    Co-Authored-By: Aidan Briggs <64035098+AJBScout@users.noreply.github.com>

--- src/main/python/db_script.py
+++ src/main/python/db_script.py
@@ -17,22 +17,32 @@ with open("order_items.csv", "w", newline="", encoding="utf-8") as item_file:
     history_writer.writerow(history_field)
 
     total_income = 0
-    order_time = datetime.datetime(2024, 1, 1)
-    for order_ID in range(120000):
+    order_time = datetime.datetime(2023, 8, 1)
+    dt = datetime.datetime.now()
+    order_ID = 0
+    #for order_ID in range(120000):
+    while(order_time < dt):
         total_cost = 0
         num_items = random.randint(1, 2)
         for item_num in range(1, num_items+1):
             option_num = random.randint(1, len(option_list)-1)
             item_list = option_list[option_num].split(",")
             item_list.insert(0, order_ID)
-            item_list.append(item_num)
-            item_list[8] = item_list[8].replace("\n", "")
+            item_list.insert(1, item_num)
+            item_list[9] = item_list[9].replace("\n", "")
             item_writer.writerow(item_list)
-            total_cost += float(item_list[8])
+            total_cost += float(item_list[9])
         history_list = [order_ID, order_time, total_cost, "383493"]
         history_writer.writerow(history_list)
 
         total_income += total_cost
 
-        time_change = datetime.timedelta(seconds=270+random.randint(-20,20))
-        order_time = order_time + time_change
+        if(order_time.date() == datetime.datetime(2024, 8, 20) or order_time.date() == datetime.datetime(2024, 1, 20)):
+            time_change = datetime.timedelta(seconds=200+random.randint(-20,20))
+            order_time = order_time + time_change
+        
+        else:
+            time_change = datetime.timedelta(seconds=270+random.randint(-20,20))
+            order_time = order_time + time_change
+
+        order_ID += 1
