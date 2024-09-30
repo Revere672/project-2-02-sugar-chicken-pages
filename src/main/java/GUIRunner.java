45f18adb0e365920ea2ff8dba0b165f9a8bf5b0d
Aidan Briggs <64035098+AJBScout@users.noreply.github.com>
2024-09-30 18:14:22 -0500

    Added scene changing
    Added a function to change between scenes

--- src/main/java/GUIRunner.java
+++ src/main/java/GUIRunner.java
@@ -1,27 +1,44 @@
+
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.sql.*;
 
 public class GUIRunner extends Application {
+
     public static boolean isManager;
+    public static HashMap<String, Scene> scenes;
+    public static Stage stage;
+    public static GUIBuilder build;
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
-
-        stage.setScene(new Scene(root));
+        scenes.put("login", new Scene(root));
+        GUIRunner.stage = stage;
+        stage.setScene(scenes.get("login"));
         stage.show();
     }
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        DBUtil.dbConnect(args[0],args[1],args[1],args[2]);
+    public void loadScenes() throws IOException {
 
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+        DBUtil.dbConnect(args[0], args[1], args[1], args[2]);
+        scenes = new HashMap<>();
+        build=new GUIBuilder(args);
         launch();
     }
 
+    public static void changeScene(String scene_Name) {
+        stage.setScene(scenes.get(scene_Name));
+        stage.show();
+    }
 }
+
