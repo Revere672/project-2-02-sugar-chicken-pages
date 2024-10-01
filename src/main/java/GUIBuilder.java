e897b64aebceed7d8c5f88483ed9a3f5740a76b7
theejoshuajg <theejoshuajg@tamu.edu>
2024-09-30 19:19:57 -0500

    switch between scenes as needed

--- src/main/java/GUIBuilder.java
+++ src/main/java/GUIBuilder.java
@@ -9,5 +9,11 @@ public class GUIBuilder {
     public GUIBuilder(String[] args) throws ClassNotFoundException, SQLException, IOException {
         Scene inventory = new Scene(FXMLLoader.load(getClass().getResource("/fxml/inventory.fxml")));
         GUIRunner.scenes.put("inventory", inventory);
+        Scene cashier1 = new Scene(FXMLLoader.load(getClass().getResource("/fxml/cashier1.fxml")));
+        GUIRunner.scenes.put("cashier1", cashier1);
+        Scene cashier2 = new Scene(FXMLLoader.load(getClass().getResource("/fxml/cashier2.fxml")));
+        GUIRunner.scenes.put("cashier2", cashier2);
+        Scene employees = new Scene(FXMLLoader.load(getClass().getResource("/fxml/employees.fxml")));
+        GUIRunner.scenes.put("employees", employees);
     }
 }
