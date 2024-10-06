ec9fa4d0a92c60e075f4935edb327164bcd75e3f
theejoshuajg <theejoshuajg@tamu.edu>
2024-10-06 00:12:09 -0500

    added build command to make jar file

---
+++ src/main/java/MainEntry.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class MainEntry {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException{
+        GUIRunner.main(args);
+    }
+}
