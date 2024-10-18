cc53b1f852195a742f7ff1b56ca2242a6613869e
Manzana475 <156945307+Manzana475@users.noreply.github.com>
2024-10-18 15:19:26 -0500

    Added Documentation

--- src/main/java/EmployeeDB.java
+++ src/main/java/EmployeeDB.java
@@ -4,8 +4,21 @@ import java.sql.SQLException;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
+/*
+ * Class used for handling database operations relating to employees and their information.
+ * Operations include: searching, updating, inserting, and returning employee information
+ */
 public class EmployeeDB {
-    
+
+    /**
+     * Searches for employees whose names match or contain the given name string in the parameter
+     * 
+     * @param name as the name or partial part of name for the employees being searched for
+     * @return an ObservableList of Employee objects that are returned from the query execution
+     * @throws SQLException if an error occurs during the query execution
+     * @throws ClassNotFoundException if the JDBC driver is not found
+     */
     public static ObservableList<Employee> find_Employee(String name) throws SQLException, ClassNotFoundException {
         String selected_Employee = "SELECT * FROM Employees WHERE Employee_name LIKE '%" + name + "%';";
 
@@ -20,6 +33,13 @@ public class EmployeeDB {
         }
     }
 
+    /**
+     * Retrieves a single employee from the given ResultSet in the parameter, along with all of its defined properties
+     * 
+     * @param result_e the ResultSet containing the employee data
+     * @return the Employee object populated with the data from the ResultSet
+     * @throws SQLException if an error occurs while processing the ResultSet
+     */
     private static Employee getRequestedEmployee(ResultSet result_e) throws SQLException{
         Employee e = null;
         if (result_e.next()) {
@@ -34,6 +54,13 @@ public class EmployeeDB {
         return e;
     }
 
+    /**
+     * Retrieves all employees that exist in the database
+     * 
+     * @return an ObservableList of all Employee objects that exist in the database
+     * @throws SQLException if an error occurs during the query execution
+     * @throws ClassNotFoundException if the JDBC driver is not found
+     */
     public static ObservableList<Employee> searchEmployees () throws SQLException, ClassNotFoundException {
         String selected_Employee = "SELECT * FROM Employees;";
         try {
@@ -46,6 +73,14 @@ public class EmployeeDB {
         }
     }
 
+    /**
+     * Converts a ResultSet of employee data into an ObservableList of Employee objects and returns it
+     * 
+     * @param result_e as the ResultSet containing employee data
+     * @return an ObservableList of Employee objects created using the data from @param result_e
+     * @throws SQLException if an error occurs while processing the ResultSet
+     * @throws ClassNotFoundException if the JDBC driver is not found
+     */
     private static ObservableList<Employee> getEmployeeList(ResultSet result_e) throws SQLException, ClassNotFoundException {
         ObservableList<Employee> eList = FXCollections.observableArrayList();//obseravble employee list(full of employee objects)
         while (result_e.next()) {
@@ -62,6 +97,15 @@ public class EmployeeDB {
         return eList;//return the elist as an observable list
     }
 
+    /**
+     * Updates the current email and active status of an employee in the database to new values
+     * 
+     * @param employee_ID as the ID of the designated employee whose attributes are to be updated
+     * @param employee_Email as the employee's new email attribute
+     * @param status as the employee's new active status 
+     * @throws SQLException if an error occurs during the update operation
+     * @throws ClassNotFoundException if the JDBC driver is not found
+     */
     public static void updateEmployeeValues(int employee_ID, String employee_Email, Boolean status) throws SQLException, ClassNotFoundException {
         //Create new string with update email
         String new_q ="UPDATE Employees SET employee_email = '" + employee_Email + "',active = " + status + " WHERE employee_ID = " + employee_ID + ";";
@@ -75,7 +119,19 @@ public class EmployeeDB {
     }
 
     //make an updateEmployeeStatus thing
-
+    
+    /**
+     * Inserts a new employee into the database, given a set of appropriate attributes
+     * 
+     * @param Employee_ID as the ID of the new employee
+     * @param name as the new employee's name
+     * @param email as the new employee's email
+     * @param role as the new employee's role
+     * @param login as the new employee's last login date
+     * @param status as the new employee's current status 
+     * @throws SQLException if an error occurs during the insert operation
+     * @throws ClassNotFoundException if the JDBC driver is not found
+     */
     public static void insertEmployee(int Employee_ID,String name, String email, String role, Date login , Boolean status) throws SQLException, ClassNotFoundException {
         //create a new querey that inserts a new employee with given attributes
         String new_q = "INSERT INTO Employees (employee_ID, employee_name, employee_email, employee_role, last_login, active) VALUES ('"+Employee_ID+"' ,'"+name+"','"+email+"','"+role+"' ,'"+login+"' ,'"+status+"');";
@@ -88,6 +144,14 @@ public class EmployeeDB {
         }
     }
 
+    /**
+     * Searches for a single employee by name and returns their information to be used for editing/updating later
+     * 
+     * @param name as the employee's name to be searched
+     * @return the Employee object containing the inquired employee's information
+     * @throws SQLException if an error occurs during the query execution
+     * @throws ClassNotFoundException if the JDBC driver is not found
+     */
     public static Employee find_edit_Employee(String name) throws SQLException, ClassNotFoundException {
         String selected_Employee = "SELECT * FROM Employees WHERE Employee_name = '" + name + "';";
 
