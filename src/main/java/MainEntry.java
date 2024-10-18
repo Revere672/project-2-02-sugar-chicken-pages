2e7b6579ebc339e1b1ff53317ff299af8be6dda4
Joshua-r1649 <113871474+theejoshuajg@users.noreply.github.com>
2024-10-18 15:52:46 -0500

    added documentation for guibuilder, login, mainentry, and menu

--- src/main/java/MainEntry.java
+++ src/main/java/MainEntry.java
@@ -1,8 +1,24 @@
 import java.io.IOException;
 import java.sql.SQLException;
 
+/**
+ * Main entry point for the application.
+ * This class is used to run the GUIRunner class.
+ * This class is needed in order to run the jar file built with gradle
+ * 
+ * @author Joshua George
+ */
 public class MainEntry {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException{
+    /**
+     * The main entry point for the application.
+     * This method launches the GUIRunner application.
+     *
+     * @param args the command line arguments.
+     * @throws ClassNotFoundException if the JDBC driver class is not found.
+     * @throws SQLException           if a database access error occurs.
+     * @throws IOException            if an I/O error occurs.
+     */
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         GUIRunner.main(args);
     }
 }
